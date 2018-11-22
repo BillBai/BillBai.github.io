46dde6f630aa5906b6d442f5dd3f59432136322f
billbai <billbai42@gmail.com>
2018-11-22 16:43:07 +0800

    decrease font size

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
         "title": "Hello World~",
-        "excerpt":"终于（又一次）搭好了 Blog ～","categories": [],
+        "excerpt":"终于（又一次）搭好了 Blog ～  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/firstpost/",
         "teaser":null},{
         "title": "准备开几个坑",
-        "excerpt":"一个学期又要过完了，想想大学还有一年半就要结束了呢~最近已经进入紧张的预复习状态，一个月后，就要迎来一个超级长的寒假，在这个寒假，准备实现几个小东西~* 一个 r5rs 标准的 scheme 解释器. 不得不说 scheme 真是简洁，spec 才50页。只是个玩具，写着玩儿~* 一个 Game Boy 模拟器！小时候觉得这些模拟都是很厉害的东西，现在想想，发现自己完全能写嘛~ 那就写一个吧。* 一个 MIPS 架构 5 级流水线 CPU，用 VHDL+FPGA 实现，正好当作数字系统设计的课程设计啦~","categories": [],
+        "excerpt":"一个学期又要过完了，想想大学还有一年半就要结束了呢~   最近已经进入紧张的预复习状态，一个月后，就要迎来一个超级长的寒假，在这个寒假，准备实现几个小东西~      一个 r5rs 标准的 scheme 解释器. 不得不说 scheme 真是简洁，spec 才50页。只是个玩具，写着玩儿~   一个 Game Boy 模拟器！小时候觉得这些模拟都是很厉害的东西，现在想想，发现自己完全能写嘛~ 那就写一个吧。   一个 MIPS 架构 5 级流水线 CPU，用 VHDL+FPGA 实现，正好当作数字系统设计的课程设计啦~  ","categories": [],
         "tags": [],
         "url": "http://localhost:4000/lets-make-something/",
         "teaser":null}]
